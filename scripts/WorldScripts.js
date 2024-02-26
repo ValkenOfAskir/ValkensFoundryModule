be1f77419d371dad7062de7ab31d6862943cf4be
Christian Seemann <falconchrisi2@gmail.com>
2024-02-26 13:34:38 +0100

    equipmentTypes need to be single words?

--- scripts/WorldScripts.js
+++ scripts/WorldScripts.js
@@ -1,6 +1,6 @@
 Hooks.on("ready", () => {
     CONFIG.DND5E.featureTypes.martialTechnique = {label:"Martial Technique"};
-    CONFIG.DND5E.equipmentTypes.armorPiece = "Armor Piece";
+    CONFIG.DND5E.equipmentTypes.armor = "Armor Piece";
     CONFIG.DND5E.consumableTypes.inspirationCard = "Inpiration Card";
     CONFIG.DND5E.abilityActivationTypes.attack = "Attack";
 })

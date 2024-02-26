83b2c5492ca679c154f1327226b9d209bcb95639
Christian Seemann <falconchrisi2@gmail.com>
2024-02-26 12:24:09 +0100

    Equipment Types and Consumable Types don't need Labels

--- scripts/WorldScripts.js
+++ scripts/WorldScripts.js
@@ -1,6 +1,6 @@
 Hooks.on("ready", () => {
     CONFIG.DND5E.featureTypes.martialTechnique = {label:"Martial Technique"};
-    CONFIG.DND5E.equipmentTypes.armorPiece = {label:"Armor Piece"};
-    CONFIG.DND5E.consumableTypes.inspirationCard = {label:"Inpiration Card"};
+    CONFIG.DND5E.equipmentTypes.armorPiece = "Armor Piece";
+    CONFIG.DND5E.consumableTypes.inspirationCard = "Inpiration Card";
     CONFIG.DND5E.abilityActivationTypes.attack = "Attack";
 })

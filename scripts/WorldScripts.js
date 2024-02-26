8d2e029b383716cb8e2f11cb35129ac5751bc3d6
Christian Seemann <falconchrisi2@gmail.com>
2024-02-26 12:17:12 +0100

    Fixed capitalization Error

--- scripts/WorldScripts.js
+++ scripts/WorldScripts.js
@@ -1,6 +1,6 @@
 Hooks.on("ready", () => {
     CONFIG.DND5E.featureTypes.martialTechnique = {label:"Martial Technique"};
-    CONFIG.DND5E.equipmentTypes.ArmorPiece = {label:"Armor Piece"};
-    CONFIG.DND5E.consumableTypes.InspirationCard = {label:"Inpiration Card"};
+    CONFIG.DND5E.equipmentTypes.armorPiece = {label:"Armor Piece"};
+    CONFIG.DND5E.consumableTypes.inspirationCard = {label:"Inpiration Card"};
     CONFIG.DND5E.abilityActivationTypes.attack = "Attack";
 })
